1f301ec38598027a11c9804e5c18c8af0261015e
Captain785 <thakurmukeshkumar785@gmail.com>
2022-12-15 10:39:04 +0530

    done some work

--- chat-analyzer.py
+++ chat-analyzer.py
@@ -1,3 +1,81 @@
 import streamlit as st
+import function as f
+import matplotlib.pyplot as plt
 
-st.title("Captain785")
+
+st.sidebar.title("Whatsapp chat analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a chat txt file")
+
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+
+    df = f.data_preprocessing(data)
+    # st.dataframe(df)
+
+    user_list = df['sender'].unique().tolist()
+    user_list.insert(0, "All")
+    selected_user = st.sidebar.selectbox("Select user", user_list)
+
+    ## Adding the button
+    if st.sidebar.button("Analyze"):
+        st.title("Analysis with repect to {}".format(selected_user))
+
+        if selected_user != "All":
+            df = df[df['sender'] == selected_user]
+        
+        ## Some variables for later use
+        words = f.total_words(df)
+
+        ## Defined four columns
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.header("Total chats")
+            st.title(df.shape[0])
+        
+        with col2:
+            st.header("Total words")
+            st.title(len(words))
+        
+        with col3:
+            st.header("Total media")
+            st.title(str(f.total_media(df)))
+        
+        with col4:
+            st.header("Total Links")
+            st.title(str(f.total_links(df)))
+        
+        ## Now adding the most busy user.
+        if selected_user == "All":
+            st.title("Top Busy Users")
+            busy_users = f.top_busy_user(df)
+            col1 , col2 = st.columns(2)
+
+            with col1:
+                plt.figure(figsize=(6,6))
+                fig, ax = plt.subplots()
+                plt.xticks(rotation='vertical')
+                ax.bar(busy_users.index, busy_users.values, color='blue')
+                st.pyplot(fig)
+            
+            with col2:
+                perc_busy_user = f.top_busy_user_percentage(df)
+                st.dataframe(perc_busy_user)
+        
+        ## Now plotting the word cloud
+        st.title("Word cloud")
+        wordcloud = f.create_wordcloud(words)        
+
+        # plot the WordCloud image                      
+        plt.figure(figsize = (8, 8), facecolor = None)
+        plt.axis("off")
+        plt.tight_layout(pad = 4)
+        fig, ax = plt.subplots()
+        ax.imshow(wordcloud)
+        st.pyplot(fig)
+
+        ## Most frequent words
+        st.title("Most frequent words")
+        freq_words = f.most_freq_words(words)
+        st.dataframe(freq_words)
--- function.py
+++ function.py
@@ -3,9 +3,15 @@ import numpy as np
 import matplotlib.pyplot  as plt
 import seaborn as sns
 import re
+import emoji
+from collections import Counter
+import warnings
+from wordcloud import WordCloud, STOPWORDS
+warnings.filterwarnings("ignore")
 
 ## Function to find mobile number and names
 def find_sender(x):
+    # type: ignore
     pattern = "^\s-\s([\w\s\d+]+):"
     res = re.search(pattern, x)
     if res == None:
@@ -14,10 +20,9 @@ def find_sender(x):
         return res[1]
 
 ## Function to preprocess the data
-def data_preprocessing(file_path):
-    ## Loading chat data
-    file = open(file_path)
-    data = file.read()
+def data_preprocessing(data):
+    # ## Loading chat data
+    # type: ignore
     pattern = "\d{1,2}/\d{1,2}/\d{1,2},\s\d{1,2}:\d{1,2}\s.M"
 
     message = re.split(pattern, data)[1:]
@@ -47,3 +52,47 @@ def data_preprocessing(file_path):
     df.drop(['date','message'], axis=1, inplace=True)
     return df
 
+def member_count(df):
+    return len(df['sender'].unique())
+
+def total_media(df):
+    id = "<media omitted>"
+    return df['text'].apply(lambda x  : x.count(id)).sum()
+
+def emoji_table(df):
+    emj = []
+    emoji_dict = emoji.get_emoji_unicode_dict('en')
+    for row in df['text']:
+        for c in row:
+            # type: ignore
+            if emoji.demojize(c) in emoji_dict:
+                emj.append(c)
+    return dict(Counter(emj).most_common(10))
+
+def total_links(df):
+    pattern = "https?:"
+    return df['text'].apply(lambda x : len(re.findall(pattern, x))).sum()
+
+def total_words(df):
+    words = []
+    for row in df['text']:
+        for word in row.split():
+            if word != '<media' and word != 'omitted>':
+                words.append(word)
+    return words
+
+def top_busy_user(df):
+    return df['sender'].value_counts().head(5)
+
+def top_busy_user_percentage(df):
+    busy_user = round(df['sender'].value_counts()/df.shape[0] * 100, 2).reset_index()
+    return busy_user.rename(columns={'index':'Sender','sender':'Percentage'})
+
+def create_wordcloud(words):
+    return WordCloud(width = 600, height =400,
+                background_color ='white',
+                stopwords = set(STOPWORDS),
+                min_font_size = 10).generate("".join(words))
+
+def most_freq_words(words):
+    return pd.DataFrame(Counter(words).most_common(20), columns=['Words', 'Frequency'])
